a85fcc3f9f607b30b6966291878f9198b60956aa
James Murphy <jmurphyinbox@gmail.com>
2021-10-11 19:41:59 -0400

    Added comments screenCapture.py

--- screenCapture.py
+++ screenCapture.py
@@ -1,3 +1,12 @@
+'''
+OverwatchCharacterDetection v1.0 - James Murphy
+Created with Python 3.9.7
+Requires installation of: mss, opencv-python, pillow
+NOTE:  
+    - Must set Overwatch Resolution to 1024 x 768
+    - Must put Overwatch Window in top left of your screen as small as possible
+    - Recommended to set the game to 30FPS for best results
+'''
 from mss import mss
 import cv2
 from PIL import Image
@@ -13,7 +22,7 @@ sct = mss()
 while 1:
     sct_img = sct.grab(win)
     img = Image.frombytes('RGB', (sct_img.size.width, sct_img.size.height), sct_img.rgb)
-    img_bgr = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR)
+    img_bgr = cv2.cvtColor(np.array(img), cv2.COLOR_RGB2BGR) 
     cv2.imshow('test', np.array(img_bgr))
     if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
